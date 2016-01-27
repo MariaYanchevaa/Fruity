55e94547e6da682b4ff2b25fa714d6ca7a892fb2
MariaYanchevaa <mimssito0o@gmail.com>
2016-01-27 19:56:27 +0200

    Ftuity

---
+++ js/functions.js
@@ -0,0 +1,16 @@
+$(function(){
+
+	$('.bxslider').bxSlider({
+		mode: 'horizontal',
+		captions: true
+	});
+
+	$('.blog-slider').bxSlider({
+		mode: 'horizontal',
+		pager: false,
+		captions: true
+	});
+
+})
+
+
